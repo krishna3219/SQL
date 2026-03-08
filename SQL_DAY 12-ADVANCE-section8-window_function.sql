52687c9ea86a6c9953959295efe683414e3bfeca
krishna <68203627+krishna3219@users.noreply.github.com>
2026-03-08 16:01:48 +0530

    Add examples for window functions in SQL
    Added examples of predefined window functions including ROW_NUMBER, RANK, DENSE_RANK, LAG, and LEAD. Updated employee salaries to demonstrate ranking differences.

--- SQL_DAY 12-ADVANCE-section8-window_function.sql
+++ SQL_DAY 12-ADVANCE-section8-window_function.sql
@@ -28,5 +28,36 @@
     CAST(salary * 100 / SUM(salary) OVER(PARTITION BY department) AS DECIMAL (10,2)) AS Percentage_Contribution_Department
     FROM EMPLOYEE;
 
+    -- PREDEFINED WINDOW FUNCTIONS:
+    -- ROW_NUMBER() , RANK() , DENSE_RANK() , NTILE(), LAG() , LEAD()
 
+        -- Scenario 1: Add row number for each employee in table.
+        -- ROW_NUMBER()
+        SELECT  ROW_NUMBER() OVER (ORDER BY fname) AS Row_Num, emp_id, fname, department, salary 
+        FROM EMPLOYEE;
 
+        -- Scenario 2: Rank employees based on their salary from highest to lowest.
+        -- RANK()
+        SELECT RANK() OVER(ORDER BY salary DESC) AS Rank_Num, emp_id, fname, department, salary
+        FROM EMPLOYEE;
+
+        -- Scenario 3: Rank employees based on their salary from highest to lowest without skipping ranks.
+        -- DENSE_RANK()
+        SELECT DENSE_RANK() OVER(ORDER BY salary DESC) AS Dense_Rank_Num, emp_id, fname, department, salary 
+        FROM EMPLOYEE;
+
+        -- update employee table with some duplicate salaries to show the difference between RANK and DENSE_RANK.
+        UPDATE EMPLOYEE
+        SET salary = 95000
+        WHERE emp_id = 110;  -- Kavy's salary updated to 95000 (same as Priya's salary)
+
+        -- Now rerun the RANK and DENSE_RANK queries to see the difference in ranking for employees with same salary.
+
+        -- Scenario 4: LAG() and LEAD()
+        -- LAG() accesses the previous row data
+        SELECT emp_id, fname, salary, LAG(salary) OVER (ORDER BY salary DESC) AS Previous_Salary
+        FROM EMPLOYEE;
+
+        -- LEAD() accesses the next row data
+        SELECT emp_id, fname, salary, LEAD(salary) OVER (ORDER BY salary DESC) AS Next_Salary
+        FROM EMPLOYEE;
